16c55e3eb9b19fc6bfd99b6de4c2faa2e6d61a82
Klára Futej <ceadis.cowlover13@gmail.com>
2024-01-14 19:48:02 +0100

    Add project files.

---
+++ Passworld2/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Passworld2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSettings(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Settings());
+        }
+
+        private async void OnSearch(object sender, EventArgs e)
+        {
+            await DisplayAlert("Good job!", "You clicked the search button!", "OK");
+        }
+    }
+}
---
+++ Passworld2/Settings.xaml.cs
@@ -0,0 +1,9 @@
+namespace Passworld2;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+}
